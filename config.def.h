89eb5495cc58cd5a8fe8fa32623910ef7c192b1c
-hasty- <tikhomirovtimofei@gmail.com>
2022-01-03 13:44:16 +0300

    Change cursor shape to underline

--- config.def.h
+++ config.def.h
@@ -149,7 +149,7 @@ static unsigned int defaultrcs = 0;
  * 7: Blinking st cursor
  * 8: Steady st cursor
  */
-static unsigned int cursorstyle = 1;
+static unsigned int cursorstyle = 3;
 static Rune stcursor = 0x2603; /* snowman (U+2603) */
 
 /*
